7fa7d46afebb1df93620240be4dafb097bb3bb67
Uzair4kf <limiaraion@gmail.com>
2022-03-18 00:50:09 +0500

    commit

---
+++ script.js
@@ -0,0 +1,51 @@
+let randomNum = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highScore = 0;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+ 
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number';
+  } else if (guess === randomNum) {
+    document.querySelector('.message').textContent = 'Correct Number';
+    document.querySelector('body').style.backgroundColor = 'Green';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = randomNum;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess > randomNum) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < randomNum) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  randomNum = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('body').style.backgroundColor = 'Black';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+});
