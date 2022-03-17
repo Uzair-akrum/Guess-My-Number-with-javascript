6b99d3c72a662b42f9bf9ccbb21e41c6fe424b0f
Uzair4kf <limiaraion@gmail.com>
2022-03-18 00:51:37 +0500

    commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let randomNum = Math.trunc(Math.random() * 20) + 1;
 
-let score = 20;
+let score = 20;   
 let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
